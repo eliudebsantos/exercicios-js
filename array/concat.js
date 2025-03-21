41c309915ad2264cc50d7ed1ffa852c9b4e89bc9
Eliude-Barbosa-Santos <151584661+eliudebsantos@users.noreply.github.com>
2025-03-20 21:10:46 -0300

    array

---
+++ array/concat.js
@@ -0,0 +1,9 @@
+const filhos = ['Kaka', 'Calleri']
+const filhas = ['Isa', 'Dahiana']
+const teste = [1,2,3]
+
+const todos = filhos.concat(filhas, teste)
+
+console.log(todos)
+
+console.log([].concat('a', 'b', [1, 2], [3, 4], [[5,7]]))
--- array/reduce2.js
+++ array/reduce2.js
@@ -1,47 +1,54 @@
 const alunos = [
-    {nome: 'João', notas: 7.3, bolsista: false},
-    {nome: 'Maria', notas: 9.2, bolsista: true},
+    {nome: 'João', notas: 7.3, bolsista: true},
+    {nome: 'Maria', notas: 9.2, bolsista: false},
     {nome: 'Zé', notas: 9.8, bolsista: false},
-    {nome: 'Ana', notas: 8.7, bolsista: true},
+    {nome: 'Ana', notas: 8.7, bolsista: false},
 ]
+
+console.log(alunos.map( e => e.bolsista).reduce((function (acu, atual) {
+    return acu && atual ? 'Sim todos são bolsistas' : 'Nem todos são bolsistas'
+})))
 
 //Desafio 1: todos os alunos são bolsista?
 
-const todosBolsistas = function (resultados, bolsista) {
+/*const todosBolsistas = function (resultados, bolsista) {
     return resultados && bolsista
 }
 console.log(alunos.map(e => e.bolsista).reduce(todosBolsistas()))
+*/
+
+/*const teste1 = alunos.map(function (e){
+    return e.bolsista
+    
+})
+console.log(teste1)
+
+const teste2 = teste1.reduce(function (acu, atual)  {
+    return acu && atual
+})
+
+console.log(teste2?'sim':'nao')*/
+
+/*const teste = alunos.reduce( (acumulador, atual) => {
+    if (atual.bolsista == false){
+        acumulador = false
+    }
+
+    return acumulador
+
+}, true)
+
+console.log(teste)*/
 
 //Desafio 2: Algum aluno é bolsista
 
-/*console.log(alunos.map(function (e){
-    const teste = [e.nome, e.bolsista]
-    return teste
-}).reduce(function(c){
-    const teste2 = []
-    if(c){
-        teste2.push[c]
+/*const teste = alunos.reduce(function (acumulador, atual) {
+    if (atual.bolsista == true){
+        acumulador = true
     }
-    return teste2
-}))
-
-/*const teste = alunos.map(e => e.nome, e.bolsista).reduce(function (acumulador, atual){
-    if (acumulador){
-
-    }
-})
+    return `${acumulador == true ? 'Sim tem pelo menos um aluno bolsista' : 'Não tem nenhum aluno bolsista'}`
+}, false)
 
 console.log(teste)*/
 
-
-/*const bolsist = alunos.map(e => e.bolsista)
-
-const teste = bolsist.reduce(function(acu, atu) {
-    if(acu){
-        return 'Todos são bolsistas'
-    } else {
-        return `${acu} não é bolsista`
-    }
-})
-
-console.log(teste)*/
+console.log(alunos.map( e => e.bolsista).reduce((acu, atual) => acu || atual))
