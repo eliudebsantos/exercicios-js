e545be950d6fb3cc224e2c479ef2a80634ddca7d
Eliude-Barbosa-Santos <151584661+eliudebsantos@users.noreply.github.com>
2025-05-20 21:30:27 -0300

    terça 20/05/2025

---
+++ ListaDeExercicios2/exe24.js
@@ -0,0 +1,24 @@
+/*Desenvolva uma função que recebe um 
+caractere e uma string como parâmetros 
+e retorne a quantidade de vezes
+que o caractere se repete na string. 
+A função deverá ser case-sensitive, ou seja, 
+irá diferenciar maiúsculas de
+minúsculas.
+Exemplos:
+contarCaractere("r", "A sorte favorece os audazes") // retornará 2
+contarCaractere("c", "Conhece-te a ti mesmo") // retornará 1*/
+
+function contarCaractere (letra, frase){
+    let contador = 0
+    const teste = frase.split("")
+    for (let i = 0; i < teste.length; i++){
+        if (teste[i] === letra){
+            contador++
+        }
+    }
+    return contador
+}
+
+console.log(contarCaractere("r", "A sorte favorece os audazes"))
+console.log(contarCaractere("c", "Conhece-te a ti mesmo"))
